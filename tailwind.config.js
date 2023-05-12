1de490f562b68150df5ff47278a9be27cbc9c1f7
Miguelzvd <dilsonmiguel25@gmail.com>
2023-05-12 13:50:59 -0300

    update tela login-copy e tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         primaryLight: "hsl(205, 88%, 30%)",
         primary: "hsl(205, 88%, 22%)",
         primaryDark: "hsl(205, 88%, 16%)",
       },
       width: {
-        '128': '32rem',
-        '140': '40rem',
-        '192': '48rem',
+        128: "32rem",
+        140: "40rem",
+        192: "48rem",
       },
     },
   },
-  plugins: [],
-}
-
+};
