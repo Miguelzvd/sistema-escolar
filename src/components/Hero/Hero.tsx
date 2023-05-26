683913bb9ac90e9e37c9fe7279e0c012956e715f
Edilson Miguel De Azevedo Filho <dilsonmiguel25@gmail.com>
2023-05-26 14:03:02 -0300

    update

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <main className="text-black bg-slate-500 mx-auto mt-52 max-w-[800px] w-full h-fit flex flex-col text-center">
+    <main className="text-black bg-slate-500 mx-auto mt-52 max-w-[800px] w-full h-screen flex flex-col text-center">
         <p className="text-primaryDark font-bold p-2">ESCOLA MHR</p> 
         <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold">Uma escola que se preocupa com seu filho.</h1>
         <div>
