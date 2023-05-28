0dbf3226872d66dd4a265529d68c76af3cc55e30
Miguel De Azevedo Filho <dilsonmiguel25@gmail.com>
2023-05-27 23:19:19 -0300

    update home page

--- src/components/Hero/Card.tsx
+++ src/components/Hero/Card.tsx
@@ -2,11 +2,12 @@ type Props = {
   children: React.ReactNode;
   text: string;
   title: string;
+  Shadowcolor: string;
 }
 
-export function Card({ children, text, title }: Props) {
+export function Card({ children, text, title, Shadowcolor }: Props) {
   return (
-    <div className="w-full sm:w-fit h-fit p-4 bg-[#353536] rounded-lg shadow-2xl hover:shadow-sky-600 cursor-default">
+    <div className= {`w-full sm:w-fit h-70 p-4 bg-[#353536] rounded-lg shadow-2xl cursor-default duration-300 ${Shadowcolor}`}>
       <div className="w-[90%] flex flex-col text-justify gap-4">
         {/* Logo and title */}
         <div className="flex flex-col text-justify gap-2 text-xl font-bold text-white">
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,12 @@
 import Typed from "react-typed";
-import { VitejsIcon, ReactIcon, NodejsIcon } from "../../iconComponents/HomeScreen";
+import { VitejsIcon, ReactIcon, NodejsIcon, TailwindIcon, MySQLIcon } from "../../iconComponents/HomeScreen";
 import { Card } from "./Card";
 
 export default function Hero() {
   return (
     <main className="text-black max-w-[850px] w-full mt-[100px] h-full mx-auto flex text-center flex-col justify-start">
       <section className="font-bold text-white">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl p-4">
+        <h1 className="text-4xl md:text-5xl p-4">
           Sistema de Gerenciamento Escolar
         </h1>
         <div className="text-xl sm:text-2xl md:text-3xl">
@@ -22,49 +22,54 @@ export default function Hero() {
         </div>
       </section>
 
-      <section className="mt-32 w-full h-fit flex flex-col items-center gap-12">
-        <h1 className="text-5xl font-bold font-roboto text-white">Tecnologias utilizadas</h1>
+      <section className="mt-32 mb-8 w-full h-fit flex flex-col items-center gap-12 ">
+        <h1 className="text-3xl md:text-4xl font-bold font-roboto text-white">Tecnologias utilizadas</h1>
         {/* Card Container */}
-        <div className="w-full grid grid-flow-row grid-cols-1 sm:grid-cols-3 gap-4 justify-items-center duration-300">
+        <div className="w-full grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 duration-300">
           
           {/* Card */}
           <Card
+            Shadowcolor="hover:shadow-yellow-500"
             title="Vitejs"
             text="Vite.js é um construtor de aplicações web extremamente rápido e leve, que oferece uma experiência de desenvolvimento moderna e eficiente."
           >
-            <VitejsIcon height={20} width={20} />
+            <VitejsIcon height={25} width={25} />
           </Card>
 
           {/* Card */}
           <Card
+            Shadowcolor="hover:shadow-sky-500"
             title="React"
             text="React é uma biblioteca JavaScript popular para criar interfaces de usuário interativas e reutilizáveis, amplamente usada no desenvolvimento web."
           >
-            <ReactIcon width={20} height={20} />
+            <ReactIcon width={25} height={25} />
           </Card>
 
           {/* Card */}
           <Card
-            title="Vitejs"
-            text="Vite.js é um construtor de aplicações web extremamente rápido e leve, que oferece uma experiência de desenvolvimento moderna e eficiente."
+            Shadowcolor="hover:shadow-indigo-800"
+            title="Tailwind"
+            text="Tailwind é um framework CSS altamente configurável e de baixo nível que permite estilizar rapidamente elementos de interface."
           >
-            <NodejsIcon height={20} width={20} />
+            <TailwindIcon height={25} width={25} />
           </Card>
 
           {/* Card */}
           <Card
-            title="Vitejs"
-            text="Vite.js é um construtor de aplicações web extremamente rápido e leve, que oferece uma experiência de desenvolvimento moderna e eficiente."
+            Shadowcolor="hover:shadow-lime-400"
+            title="Nodejs"
+            text="Node.js é um interpretador JavaScript rápido e versátil, ideal para trabalhar com processos, arquivos, soquetes de rede e APIs de servidor."
           >
-            <NodejsIcon height={20} width={20} />
+            <NodejsIcon height={25} width={25} />
           </Card>
 
           {/* Card */}
           <Card
-            title="Vitejs"
-            text="Vite.js é um construtor de aplicações web extremamente rápido e leve, que oferece uma experiência de desenvolvimento moderna e eficiente."
+            Shadowcolor="hover:shadow-sky-700"
+            title="MySQL"
+            text="MySQL é um sistema de gerenciamento de banco de dados relacional, que oferece uma estrutura robusta e eficiente para armazenar e gerenciar dados em aplicações."
           >
-            <NodejsIcon height={20} width={20} />
+            <MySQLIcon height={25} width={25} />
           </Card>
 
           
