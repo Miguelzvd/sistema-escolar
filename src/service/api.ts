9229ad1382f7fcb2da70a9a047ad98c3b355cdd3
Miguel De Azevedo Filho <dilsonmiguel25@gmail.com>
2023-06-13 23:19:25 -0300

    update login

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_APP_API,
-  });
+});
   
 export const  getData = async () => {
 
   try {
-    const response = await axios.get("http://localhost:3000/api/login");
+    const response = await axios.post("http://localhost:3000/api/login");
     return response.data.result;
   }
   catch (error) {
